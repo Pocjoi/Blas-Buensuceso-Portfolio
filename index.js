38ead1b5b0fd548e01a69a5341d881088f2892f6
Pocjoi <143832642+Pocjoi@users.noreply.github.com>
2024-05-02 10:40:16 -0700

    hell ono

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 function myFunction() {
   document.getElementById("Start").style.display = "none";
-  document.getElementById("mainMenu").style.display = "block";
+  document.getElementById("mainMenu").style.display = "flex";
 }
 
 function myFunction1() {
   document.getElementById("mainMenu").style.display = "none";
-  document.getElementById("Start").style.display = "block";
+  document.getElementById("Start").style.display = "flex";
 }
 
 function toggleDiv(divId) {
