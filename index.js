2be7f7a6925ec67529618f63d4df827c5ba13f7c
Pocjoi <143832642+Pocjoi@users.noreply.github.com>
2024-05-03 09:05:00 -0700

    i wanna diiiiiiiiiiie

--- index.js
+++ index.js
@@ -11,12 +11,3 @@ function myFunction1() {
   document.getElementById("mainMenu").style.display = "none";
   document.getElementById("Start").style.display = "flex";
 }
-
-function toggleDiv(divId) {
-  var div = document.getElementById(divId);
-  if (div.style.display === "none") {
-    div.style.display = "block";
-  } else {
-    div.style.display = "none";
-  }
-}
