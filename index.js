83e0df950c84a168dab2b937d9b27cf2b7ec9008
Pocjoi <143832642+Pocjoi@users.noreply.github.com>
2024-05-09 10:40:28 -0700

    FCKING YESSSSSS

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-function darkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
 function myFunction() {
   document.getElementById("Start").style.display = "none";
   document.getElementById("mainMenu").style.display = "flex";
